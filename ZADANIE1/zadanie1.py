072be29688fc8f784ade7c725d0b590dd9b2dea1
Paulina Andrychiewicz <paulina.andrychiewicz@student.uj.edu.pl>
2025-11-16 18:06:18 +0100

    zadania 1 i 2 skonczone

--- ZADANIE1/zadanie1.py
+++ ZADANIE1/zadanie1.py
@@ -1,5 +1,34 @@
 def dodaj_element(wejscie):
-    # może warto zdefiniować zagnieżdżoną funkcję
+    
+    def znajdz_max_glebokosc(element, glebokosc=0):
+        max_glebokosc = glebokosc if isinstance(element, list) else glebokosc - 1
+        if isinstance(element, (list, tuple)):
+            for i in element:
+                if isinstance(i, (list, tuple, dict)):
+                    max_glebokosc = max(max_glebokosc, znajdz_max_glebokosc(i, glebokosc + 1))
+        elif isinstance(element, dict):
+            for wartosc in element.values():
+                if isinstance(wartosc, (list, tuple, dict)):
+                    max_glebokosc = max(max_glebokosc, znajdz_max_glebokosc(wartosc, glebokosc + 1))   
+        return max_glebokosc                         
+        
+    def dodaj_na_glebokosci(element, docelowa_glebokosc, aktualna_glebokosc=0):
+        if aktualna_glebokosc == docelowa_glebokosc:
+            if isinstance(element, list):
+                nowa_wartosc = element[-1] + 1 if element else 1
+                element.append(nowa_wartosc)
+            return
+        if isinstance(element, (list,tuple)):
+            for i in element:
+                if isinstance(i, (list, tuple, dict)):
+                    dodaj_na_glebokosci(i, docelowa_glebokosc, aktualna_glebokosc + 1)
+        elif isinstance(element, dict):
+            for wartosc in element.values():
+                if isinstance(wartosc, (list, tuple, dict)):
+                        dodaj_na_glebokosci(wartosc, docelowa_glebokosc, aktualna_glebokosc + 1)   
+        
+    maksymalna_glebokosc = znajdz_max_glebokosc(wejscie)
+    dodaj_na_glebokosci(wejscie, maksymalna_glebokosc)    
 
     return wejscie
 
@@ -9,4 +38,4 @@ if __name__ == '__main__':
      "hello", 3, [4, 5], 5, (6, (1, [7, 8]))
     ]
     output_list = dodaj_element(input_list)
-    print(input_list)   
+    print(output_list)   
